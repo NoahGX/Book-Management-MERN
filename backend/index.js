d17ef0653a33aaa3961e352fbd53f9831e38776a
NoahGX <noahjgarcia2019@gmail.com>
2024-09-10 15:22:59 -0700

    Add files via Git

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
+import mongoose from "mongoose";
 import { PORT } from "./config.js";
+import { mongoDBURL } from "../.env";
 
 const app = express();
 
@@ -8,6 +10,12 @@ app.get('/', (request, response) => {
     return response.status(200).send('Welcome');
 })
 
-app.listen(PORT, () => {
-    console.log(`App is listening to PORT: ${PORT}`);
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to PORT: ${PORT}`);
+        });
+    }).catch((error) => {
+    console.log(error);
 });
