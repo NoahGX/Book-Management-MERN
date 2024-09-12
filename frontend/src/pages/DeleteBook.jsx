ae28f72a9517d520a77b3701a4c88021da1c2ee6
NoahGX <noahjgarcia2019@gmail.com>
2024-09-12 00:56:30 -0700

    Update

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -17,7 +17,7 @@ const DeleteBook = () => {
       .delete(`http://localhost:3000/books/${id}`)
       .then(() => {
         setLoading(false);
-        enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
+        enqueueSnackbar('Book Deleted Successfully.', { variant: 'success' });
         navigate('/');
       })
       .catch((error) => {
@@ -34,14 +34,12 @@ const DeleteBook = () => {
       <h1 className='text-3xl my-4'>Delete Book</h1>
       {loading ? <Spinner /> : ''}
       <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
-        <h3 className='text-2xl'>Are You Sure You want to delete this book?</h3>
+        <h3 className='text-2xl'>Are You Sure?</h3>
 
         <button
-          className='p-4 bg-red-600 text-white m-8 w-full'
-          onClick={handleDeleteBook}
-        >
-          Yes, Delete it
-        </button>
+        className='p-4 bg-red-600 text-white m-8 w-full'
+        onClick={handleDeleteBook}>
+          Yes, Delete</button>
       </div>
     </div>
   )
