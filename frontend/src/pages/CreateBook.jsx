646488464ebbe8c6209b75e4dfcf0921413eee50
NoahGX <noahjgarcia2019@gmail.com>
2024-09-12 18:10:19 -0700

    Update

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -14,11 +14,18 @@ const CreateBook = () => {
   const { enqueueSnackbar } = useSnackbar();
 
   const handleSaveBook = () => {
-    const data = {
-      title,
-      author,
-      publishYear,
-    };
+    // Basic validation
+    if (!title || !author || !publishYear) {
+      enqueueSnackbar('All fields are required', { variant: 'warning' });
+      return;
+    }
+  
+    if (isNaN(publishYear) || publishYear < 0) {
+      enqueueSnackbar('Publish Year must be a valid positive number', { variant: 'warning' });
+      return;
+    }
+  
+    const data = { title, author, publishYear };
     setLoading(true);
     axios
       .post('http://localhost:3000/books', data)
@@ -29,17 +36,16 @@ const CreateBook = () => {
       })
       .catch((error) => {
         setLoading(false);
-        // alert('An error happened. Please Chack console');
-        enqueueSnackbar('Error', { variant: 'error' });
+        enqueueSnackbar('Error creating the book', { variant: 'error' });
         console.log(error);
       });
-  };
+  };  
 
   return (
     <div className='p-4'>
-      <BackButton />
+      <BackButton/>
       <h1 className='text-3xl my-4'>Create Book</h1>
-      {loading ? <Spinner /> : ''}
+      {loading ? <Spinner/> : ''}
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Title</label>
@@ -47,8 +53,7 @@ const CreateBook = () => {
             type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2 w-full'
-          />
+            className='border-2 border-gray-500 px-4 py-2 w-full'/>
         </div>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Author</label>
@@ -56,8 +61,7 @@ const CreateBook = () => {
             type='text'
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
-          />
+            className='border-2 border-gray-500 px-4 py-2  w-full '/>
         </div>
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
@@ -65,8 +69,7 @@ const CreateBook = () => {
             type='number'
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
-            className='border-2 border-gray-500 px-4 py-2  w-full '
-          />
+            className='border-2 border-gray-500 px-4 py-2  w-full '/>
         </div>
         <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
           Save
