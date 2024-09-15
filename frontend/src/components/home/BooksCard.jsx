8475c86c850ba41b7afc4b26ccf3fac9a4337e39
NoahGX <noahjgarcia2019@gmail.com>
2024-09-14 17:25:52 -0700

    Update

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,19 +1,40 @@
-import { Link } from 'react-router-dom';
-import { PiBookOpenTextLight } from 'react-icons/pi';
-import { BiUserCircle } from 'react-icons/bi';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineDelete } from 'react-icons/md';
+import React, { memo } from 'react';
+ // Import PropTypes for runtime type checking
+import PropTypes from 'prop-types';
 import BookSingleCard from './BookSingleCard';
 
-const BooksCard = ({ books }) => {
+// Renders a grid of BookSingleCard components, one for each book in the `books` array
+// The component is memoized to avoid unnecessary re-renders if props haven't changed
+const BooksCard = memo(({ books }) => {
+  // If no books are available, notify user
+  if (!books?.length) {
+    return <div>No books available</div>;
+  }
+
+  // Return the grid layout with BookSingleCard components for each book
   return (
-    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
       {books.map((item) => (
+        // Key is important for React to track and optimize re-rendering
         <BookSingleCard key={item._id} book={item} />
       ))}
     </div>
   );
+});
+
+// PropTypes for runtime validation of component props
+BooksCard.propTypes = {
+  // The `books` prop is required and must be an array of objects with the fields below
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      // Validate that id, title, and author are required strings
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      // Validate that yearPublished is a number
+      yearPublished: PropTypes.number,
+    })
+  ).isRequired,
 };
 
 export default BooksCard;
